f7d6806736ee1bd9c57799627facc337ddbfc12b
victorcv92 <johan.calderonva@gmail.com>
2024-04-18 16:46:40 -0500

    Style

--- blogfront/blogWeb/src/app/app.component.ts
+++ blogfront/blogWeb/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './structure/navbar/navbar.component';
-import { FooterComponent } from './structure/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
